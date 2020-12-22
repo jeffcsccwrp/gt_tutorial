2617a45fe4be045d4efce9f7debd3202277f017a
Jeff Chokry <jeffchokry@Jeffs-MacBook-Pro.local>
2020-12-22 15:41:21 -0800

    2nd commit

--- gt_script.R
+++ gt_script.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(lubridate)
 library(gt)
 library(paletteer)
+library(scales)
 
 #look at dataset description
 ?pizzaplace
@@ -25,4 +26,54 @@ pizza_money <- pizza %>%
   mutate(type = factor(type, levels = c("classic", "veggie", "chicken", "supreme"))) %>%  #this step relevels pizza types
   summarize(sold = n(), profit = sum(price)) %>% 
   ungroup() #always ungroup data or this will carry over to following code and mess things up
+
+
+pizza_table <- pizza_money %>% 
+  filter(month_num > 9) %>% 
+  mutate(month_name = case_when(month_num == 10 ~ "October", 
+                                month_num == 11 ~ "November",
+                                month_num == 12 ~ "December")) %>% 
+  gt(groupname_col = "month_name", rowname_col = "type") %>% 
+  cols_hide(columns = vars(month_num)) %>% 
+  fmt_currency(columns = vars(profit), currency = "USD") %>% 
+  tab_header(title = "Q4 Monthly Pizza Sales",
+             subtitle = "by JC") %>% 
+  tab_source_note(md("More info can be found at `?pizzaplace`")) %>% 
+  cols_label(sold = "Pizzas Sold",
+             profit = "Profits") %>% 
+  tab_stubhead(label = "Month") %>% 
+  tab_spanner(label = "Pizzas Sold + Revenue",
+              columns = vars(sold, profit)) %>%  #need to refer to the OG name, not renames
+  summary_rows(groups = T,
+               columns = vars(profit),
+               fns = list("TOTAL" = "sum"),
+               formatter = fmt_currency, currency = "USD", 
+               use_seps = T) %>% 
+  grand_summary_rows(
+    columns = vars(profit),
+    fns = list("GRAND TOTAL" = "sum"), 
+    formatter = fmt_currency, currency = "USD",
+    use_seps = T) %>% 
+  data_color(
+    columns = vars(profit),
+    colors = scales::col_numeric(
+      palette = paletteer::paletteer_d(
+        palette = "ggsci::teal_material") %>% 
+        as.character(),
+      domain = NULL),
+    alpha = 0.75) 
+
+pizza_table
+
+gtsave(pizza_table, "pizza_table.png")
+
+
+
+
+
+
+
+
+
+
 
